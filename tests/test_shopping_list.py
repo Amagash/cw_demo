917da648e20d410d32086a2e5a30600b06d3fc5d
Amagash <tiffanysouterre@gmail.com>
2023-09-18 02:11:31 +0200

    test shopping list at cloudX dev week

--- tests/test_shopping_list.py
+++ tests/test_shopping_list.py
@@ -2,67 +2,30 @@ import unittest
 from module.shopping_list import ShoppingList
 
 class ShoppingListTest(unittest.TestCase):
-    def test_init(self):
-        shopping_list = ShoppingList()
-        self.assertEqual(shopping_list.items, [])
-
     def test_add_item(self):
         shopping_list = ShoppingList()
-        shopping_list.add_item("Banana", 1, 1.5, "AAAAAAAA")
+        shopping_list.add_item("123456AB", "Milk", 2.50, 1)
         self.assertEqual(shopping_list.items, [
-            {
-                "name": "Banana",
-                "quantity": 1,
-                "price": 1.5,
-                "reference": "AAAAAAAA"
-            }])
-
-    def test_remove_item(self):
-        shopping_list = ShoppingList()
-        shopping_list.add_item("Banana", 1, 1.5, "AB123456")
-        shopping_list.add_item("Orange", 2, 1.2, "AB1834F6")
-        shopping_list.remove_item("Banana")
-        self.assertEqual(shopping_list.items, [
-            {
-                "name": "Orange",
-                "quantity": 2,
-                "price": 1.2,
-                "reference": "AB1834F6"
-            }])
-
+            {"barecode": "123456AB", "name": "Milk", "price": 2.50, "quantity": 1}
+        ])
+     
     def test_add_multiple_items(self):
         shopping_list = ShoppingList()
         list_of_items = [
-            {"name": "Banana", "quantity": 1,"price": 1.5, "reference": "AB123456"},
-            {"name": "Orange", "quantity": 2,"price": 1.2, "reference": "AB1834F6"},
-            {"name": "Apple", "quantity": 3,"price": 1.1, "reference": "A418C4F4"},
-            {"name": "Mango", "quantity": 4,"price": 1.3, "reference": "A418D454"},
-            {"name": "Pineapple", "quantity": 5,"price": 1.4, "reference": "A418E4F4"},
-            {"name": "Grapes", "quantity": 6,"price": 1.5, "reference": "A418F4F4"},
-            ]
-        
+            {"barecode": "123456AB", "name": "Milk", "price": 2.50, "quantity": 1},
+            {"barecode": "12ER56AB", "name": "Bread", "price": 1.50, "quantity": 2},
+            {"barecode": "12ER575B", "name": "Eggs", "price": 0.50, "quantity": 3}
+        ]
+
         shopping_list.add_multiple_items(list_of_items)
         self.assertEqual(shopping_list.items, list_of_items)
 
-    def test_is_valid_reference(self):
-        valid_references = [
-            "AB123456",
-            "AB1834F6",
-            "A418C4F4",
-            "A418D454"
-            ]
-        
-        # invalide references can be longer or shorter than 8 characters
-        # have lowercase letters and contains special characters
-        invalid_references = [
-            "AB1r3457",
-            "AB1234567",
-            "AB12345?"
-            ]
+    def test_is_valid_barecode(self):
+        valid_barecodes = ["123456AB", "12ER56AB", "12ER575B"]
+        invalid_barecodes = ["123456AB1", "12ER56A", "12ER575?"]
 
-        for reference in valid_references:
-            self.assertTrue(ShoppingList.is_valid_reference(self, reference))
+        for barecode in valid_barecodes:
+            self.assertTrue(ShoppingList.is_valid_barecode(self, barecode))
 
-        for reference in invalid_references:
-            self.assertFalse(ShoppingList.is_valid_reference(self, reference))
-
+        for barecode in invalid_barecodes:
+            self.assertFalse(ShoppingList.is_valid_barecode(self, barecode))
