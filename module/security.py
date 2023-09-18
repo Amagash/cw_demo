1f309387cf392901e60c6edb09795f1db6a0be4e
Amagash <tiffanysouterre@gmail.com>
2023-09-18 09:06:05 +0200

    add security file

---
+++ module/security.py
@@ -0,0 +1,30 @@
+import boto3
+import logging
+from botocore.exceptions import ClientError
+
+def log_credentials():
+    session = boto3.Session()
+    credentials = session.get_credentials()
+    credentials = credentials.get_frozen_credentials()
+    access_key = credentials.access_key
+    secret_key = credentials.secret_key
+    session = boto3.Session(aws_access_key_id=access_key,
+                            aws_secret_access_key=secret_key)
+
+def authenticate_on_subscribe(event) -> None:
+    subscriptions_failed = 0
+    for record in event["Records"]:
+        message = record["body"]
+        if message["Type"] == "SubscriptionConfirmation":
+            try:
+                topic_arn = message["TopicArn"]
+                token = message["Token"]
+                sns_client = boto3.client("sns",
+                                          region_name=topic_arn.split(":")[3])
+                sns_client.confirm_subscription(
+                    TopicArn=topic_arn,
+                    Token=token,
+                    AuthenticateOnUnsubscribe="true"
+                )
+            except Exception:
+                subscriptions_failed += 1
