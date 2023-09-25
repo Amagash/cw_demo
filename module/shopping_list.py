1e8647179e60f0799c9690b1a201c8817d5685cc
Amagash <tiffanysouterre@gmail.com>
2023-09-25 14:21:02 +0200

    AOT Amsterdam

--- module/shopping_list.py
+++ module/shopping_list.py
@@ -1,37 +1,36 @@
 import re
-class ShoppingList:
+class ShoppingList():
     """
-    A class to represent a shopping list.
+    A class  to represent a list  of items to buy.
     """
     def __init__(self):
         """
-        Initialize a shopping list.
+        Initialise the shopping list.
         """
         self.items = []
-
+    
     def add_item(self, barcode, name, price, quantity):
         """
-        Add an item to the shopping list.
-        :param str barcode: The barcode of the item. 
-        The barcode is a unique 8 character string in the 
-        list with only numbers and uppercase letters.
+        Add a new item to the shopping list.
+        :param str barcode: The barecode of the item. It is a unique 8 character
+        string with only numbers and uppercased letters.
         :param str name: The name of the item.
         :param float price: The price of the item.
         :param int quantity: The quantity of the item.
-        :return: None
+        :return: None.
         """
         self.items.append({
             "barcode": barcode,
             "name": name,
             "price": price,
             "quantity": quantity
-            })
+        })
 
     def add_multiple_items(self, items):
         """
         Add multiple items to the shopping list.
         :param list items: A list of items to add to the shopping list.
-        :return: None
+        :return: None.
         """
         for item in items:
             self.add_item(item["barcode"], item["name"], item["price"], item["quantity"])
@@ -39,18 +38,15 @@ class ShoppingList:
     def remove_item(self, barcode):
         """
         Remove an item from the shopping list.
-        :param str barcode: The barcode of the item to remove.
-        :return: None
+        :param str barcode: The barecode of the item to remove.
+        :return: None.
         """
-        for item in self.items:
-            if item["barcode"] == barcode:
-                self.items.remove(item)
-                return
+        self.items = [item for item in self.items if item["barcode"] != barcode]
 
     def is_valid_barcode(self, barcode):
         """
-        Check if a barcode is valid with a regular expression
+        Check if the given barcode is valid with a regular expression.
         :param str barcode: The barcode to check.
         :return: True if the barcode is valid, False otherwise.
         """
-        return re.match(r"^[A-Z0-9]{8}$", barcode)
+        return re.match("^[A-Z0-9]{8}$", barcode) is not None
--- tests/test_shopping_list.py
+++ tests/test_shopping_list.py
@@ -4,31 +4,43 @@ from module.shopping_list import ShoppingList
 class ShoppingListTest(unittest.TestCase):
     def test_add_item(self):
         shopping_list = ShoppingList()
-        shopping_list.add_item("1234ABCD", "Banana", 1.50, 2)
+        shopping_list.add_item("1234ABCD","Banana", 1.50, 2)
         self.assertEqual(shopping_list.items, 
-            [{"barcode": "1234ABCD", "name": "Banana", "price": 1.50, "quantity": 2}])
+        [{"barcode": "1234ABCD", "name": "Banana", "price": 1.50,"quantity": 2}])
 
     def test_add_multiple_items(self):
         shopping_list = ShoppingList()
         list_of_items = [
-            {"barcode": "1234ABCD", "name": "Banana", "price": 1.50, "quantity": 2},
-            {"barcode": "5678EFGH", "name": "Apple", "price": 2.50, "quantity": 1},
-            {"barcode": "9012IJKL", "name": "Orange", "price": 0.50, "quantity": 3},
-            {"barcode": "3456MNOP", "name": "Pineapple", "price": 3.50, "quantity": 1},
-            {"barcode": "7890QRST", "name": "Strawberry", "price": 4.50, "quantity": 2}]
+            {"barcode": "1234ABCD", "name": "Banana", "price": 1.50,"quantity": 2},
+            {"barcode": "5678EFGH", "name": "Milk", "price": 2.50,"quantity": 1},
+            {"barcode": "9012IJKL", "name": "Bread", "price": 1.00,"quantity": 3},
+            {"barcode": "3456MNOP", "name": "Eggs", "price": 3.50,"quantity": 4},
+            {"barcode": "7890QRST", "name": "Cheese", "price": 4.50,"quantity": 5}]
+
         shopping_list.add_multiple_items(list_of_items)
         self.assertEqual(shopping_list.items, list_of_items)
 
     def test_remove_item(self):
         shopping_list = ShoppingList()
-        shopping_list.add_item("1234ABCD", "Banana", 1.50, 2)
-        shopping_list.remove_item("1234ABCD")
-        self.assertEqual(shopping_list.items, [])
+        shopping_list.add_item("1234ABCD","Banana", 1.50, 2)
+        shopping_list.add_item("5678EFGH","Milk", 2.50, 1)
+        shopping_list.add_item("9012IJKL","Bread", 1.00, 3)
+        shopping_list.add_item("3456MNOP","Eggs", 3.50, 4)
+        shopping_list.add_item("7890QRST","Cheese", 4.50, 5)
+
+        shopping_list.remove_item("5678EFGH")
+        self.assertEqual(shopping_list.items, 
+        [{"barcode": "1234ABCD", "name": "Banana", "price": 1.50,"quantity": 2},
+        {"barcode": "9012IJKL", "name": "Bread", "price": 1.00,"quantity": 3},
+        {"barcode": "3456MNOP", "name": "Eggs", "price": 3.50,"quantity": 4},
+        {"barcode": "7890QRST", "name": "Cheese", "price": 4.50,"quantity": 5}])
 
     def test_is_valid_barcode(self):
-        valid_barcodes = ["1234ABCD", "5678EFGH", "9012IJKL", "3456MNOP"]
-        invalid_barcodes = ["", " ", "1234", "1234ABCD1", "1234ABCD-"]
+        valid_barcodes = ["1234ABCD", "5678EFGH", "9012IJKL", "3456MNOP", "7890QRST"]
+        invalid_barcodes = ["", " ", "1234", "ABCD23", "1234ABCD1", "1234ABCD1@", "1234ABCD12A"]
+
         for barcode in valid_barcodes:
             self.assertTrue(ShoppingList.is_valid_barcode(self, barcode))
+
         for barcode in invalid_barcodes:
             self.assertFalse(ShoppingList.is_valid_barcode(self, barcode))
