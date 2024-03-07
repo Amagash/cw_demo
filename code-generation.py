ed8bd4e264d21dd2d2498ff40e99255c3ff89c6f
Amagash <tiffanysouterre@gmail.com>
2024-03-07 10:30:20 +0000

    delete code generation

--- code-generation.py
+++
@@ -1,9 +0,0 @@
-import csv 
-def open_csv(filename):
-    """
-    open a csv file and return a list of lines in alphabetical order
-    insert a period at the end of each line
-    replace duplicate lines with a single line
-    """
-    
-
