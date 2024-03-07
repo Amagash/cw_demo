25dfc08e84cd0b57e6dc3b02cf29bad157bf43a0
Amagash <tiffanysouterre@gmail.com>
2024-03-07 10:30:55 +0000

    renamed

--- module/code_scanning.py
+++
@@ -1,9 +0,0 @@
-from flask import app
-
-
-@app.route('/redirect')
-def redirect_url_noncompliant():
-    from flask import request, redirect
-    endpoint = request.args['url']
-    # Noncompliant: redirect to a user-supplied URL without sanitization.
-    return redirect(endpoint)
