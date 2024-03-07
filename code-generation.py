3e71a13570ece20532e5986d5145a46649c75eda
Amagash <tiffanysouterre@gmail.com>
2024-03-07 01:11:34 +0000

    add code_scanning

---
+++ code-generation.py
@@ -0,0 +1,9 @@
+import csv 
+def open_csv(filename):
+    """
+    open a csv file and return a list of lines in alphabetical order
+    insert a period at the end of each line
+    replace duplicate lines with a single line
+    """
+    
+
